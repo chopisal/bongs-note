2e9341582b993603344c5ecbb1f3b85ef92ca025
bong <chopisal@gmail.com>
2018-10-31 00:25:29 +0900

    mutation createNote

--- src/queries.js
+++ src/queries.js
@@ -1,9 +1,11 @@
 import gql from "graphql-tag";
+import { NOTE_FRAGMENT } from "./fragments";
 
 export const GET_NOTES = gql`
   {
     notes @client {
-      id
+      ...NoteParts
     }
   }
+  ${NOTE_FRAGMENT}
 `;
